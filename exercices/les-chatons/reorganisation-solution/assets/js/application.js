8332b324cc5e9e1ea06dc4892e3343583da6af81
Perrine Bocquin <perrine.bocquin@gmail.com>
2015-01-05 20:41:52 +0100

    Add chaton exercice for reorganisation of contente

---
+++ exercices/les-chatons/reorganisation-solution/assets/js/application.js
@@ -0,0 +1,40 @@
+
+(function () {
+    "use strict";
+
+    var $  = jQuery.noConflict();
+
+
+    $(document).ready(function() {  
+       
+        $('.diapos').imagesLoaded( function() {
+
+            // Navigation
+            
+            // ajoute un lien pour appeler le menu
+            $('.main-nav').prepend('<a href="#main-menu" class="toggle-menu">Menu</a>');
+
+            // toggle la classe qui rend le menu visible quand on clique sur ce lien
+            $('.toggle-menu').on('click', function(event) {
+                event.preventDefault();
+
+                //.main-nav
+                $(this).parent().toggleClass('menu-opened');
+            })
+
+
+            // Liste d'images
+            $('.diapos').masonry({
+                itemSelector: '.diapo'
+            });
+
+
+        });
+
+    });
+
+
+    // FUNCTIONS
+
+
+}(jQuery));
