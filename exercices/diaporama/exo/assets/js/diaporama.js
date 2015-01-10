1e93de5e4c46dc4330fad6bd8fbf591f287dc69d
Perrine Bocquin <perrine.bocquin@gmail.com>
2015-01-11 00:19:59 +0100

    Exercice swipe ok

---
+++ exercices/diaporama/exo/assets/js/diaporama.js
@@ -0,0 +1,86 @@
+
+(function () {
+    "use strict";
+
+    var $  = jQuery.noConflict();
+
+    // elements
+    var $diaporama = $('.diaporama');
+
+    // variables
+    var diaporamaCurrentImageIndex = 0;
+
+
+    $(document).ready(function() {  
+
+        // initialisation du diaporama
+        diaporamaInit();
+
+    });
+
+
+    // FUNCTIONS
+
+    function diaporamaInit() {
+
+        // positonnement initial de toutes les li
+        $('.diaporama__image', $diaporama).each( function(index) {
+            $(this).css({
+                'left': ((index - diaporamaCurrentImageIndex) * 100) + '%'
+            });
+        });
+
+
+        // Evenements
+
+        // click sur précédent / suivant
+        $('.diaporama-next').on('click', function(event) {
+            event.preventDefault();
+            diaporamaGoToNext();
+        });
+
+        $('.diaporama-prev').on('click', function(event) {
+            event.preventDefault();
+            diaporamaGoToPrev();
+        });
+
+
+        // Todo :  Swipe !
+
+        
+
+
+
+    }
+
+    function diaporamaImagesPosition (currentImageIndex) {
+
+        $('.diaporama__image', $diaporama).each(function(index){
+            $(this).stop().animate({
+                'left': ((index - diaporamaCurrentImageIndex) * 100) + '%'
+            }, 800);
+        });
+    }
+
+    function diaporamaGoToNext () {
+        diaporamaCurrentImageIndex++;
+        if(diaporamaCurrentImageIndex > $('.diaporama__image').length - 1 ) {
+            diaporamaCurrentImageIndex = 0;
+        } 
+
+        diaporamaImagesPosition(diaporamaCurrentImageIndex);
+    }
+
+    function diaporamaGoToPrev () {
+        diaporamaCurrentImageIndex--;
+        if(diaporamaCurrentImageIndex < 0) {
+            diaporamaCurrentImageIndex = $('.diaporama__image').length - 1;
+        } 
+
+        diaporamaImagesPosition(diaporamaCurrentImageIndex);
+    }
+
+
+
+
+}(jQuery));
